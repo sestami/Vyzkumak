f88de1cafdd98f182036be7c0d5f0108b79cb033
sestami <michal.sest@gmail.com>
2019-08-21 20:28:21 +0200

    Commit

--- mereni/Dobrichovice Anglicka 574/ventilace/ventilace.py
+++ mereni/Dobrichovice Anglicka 574/ventilace/ventilace.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import datetime
 from scipy.optimize import lsq_linear
-from uncertainties import ufloat
+from uncertainties import ufloat, unumpy
 import statsmodels.api as sm
+from ipdb import set_trace
 
 '''
 VYPOCET PRUTOKU VZDUCHU POMOCI METODY NEJMENSICH CTVERCU
@@ -14,18 +15,16 @@ def completion(velicina, velicina_err):
     return pd.Series(data=united, index=velicina.index, name=velicina.name)
 
 #ZADANI VSTUPNICH PARAMETRU, VYPOCET KONCENTRACI
-odezva_TD = pd.read_csv('odezva_TD_detektoru1.csv',sep=';', decimal=',',index_col=['zona', 'plyn'])
-# odezva_TD = pd.read_csv('odezva_TD_detektoru_nepreurcena.csv',sep=';', decimal=',',index_col=['zona', 'plyn'])
+odezva_TD = pd.read_csv('odezva_TD_detektoru.csv',sep=';', decimal=',',index_col=['zona', 'plyn'])
 odezva_TD = odezva_TD.swaplevel().sort_index()
-informace_plyny = pd.read_csv('plyny_informace1.csv',sep=';', decimal=',',index_col='plyn')
-# informace_plyny = pd.read_csv('plyny_informace_nepreurcena.csv',sep=';', decimal=',',index_col='plyn')
+informace_plyny = pd.read_csv('plyny_informace.csv',sep=';', decimal=',',index_col='plyn')
 
 N=len(odezva_TD.index.levels[1]) #pocet zon
 N_plyny=len(odezva_TD.index.levels[0]) #pocet pouzitych indikacnich plynu
 
 #vypocet doby mereni
-pocatek=datetime.datetime(2019, 5, 23, 12, 00)
-konec=datetime.datetime(2019, 6, 5, 10, 45)
+pocatek=datetime.datetime(2019, 6, 5, 12, 00)
+konec=datetime.datetime(2019, 6, 20, 10, 45)
 rozdil=konec-pocatek
 dT=rozdil.total_seconds()/3600 #v hod
 
@@ -57,7 +56,6 @@ R_konst=8.314462618 #univerzalni plynova konstanta [J/K/mol]
 
 #koncentrace a emise
 C=np.full((N_plyny, N), np.nan, dtype=object)
-M=np.full(N*N_plyny, np.nan, dtype=object)
 for k in np.arange(1, N_plyny+1):
     for i in np.arange(1,N+1):
         C[k-1,i-1]=R_d[k,i]/(U_R[k]*dT*60)*M_w[k]*p[k,i]/(R_konst*1000*T[k,i])
@@ -82,14 +80,18 @@ prutoky_indexy=pd.DataFrame(np.array([index1, index2]).T, index=np.arange(1,poce
 Z_n=pd.DataFrame(np.zeros((N*N_plyny, pocet_neznamych)), index=odezva_TD.index)
 Z_n.columns+=1
 Z_s=Z_n.copy()
+Z=pd.DataFrame(np.zeros((N*N_plyny, pocet_neznamych)), index=odezva_TD.index,
+               columns=np.arange(1, pocet_neznamych+1), dtype=object)
 
 for m in np.arange(1, pocet_neznamych+1):
     i, j=prutoky_indexy.loc[m]
     for k in np.arange(1,N_plyny+1):
         if i!=j:
+            Z.loc[(k, i), m]=-C.loc[k, i]
             Z_n.loc[(k, i), m]=-C.loc[k, i].n
             Z_s.loc[(k, i), m]=-C.loc[k, i].s
             if j<=N:
+                Z.loc[(k, j), m]= C.loc[k, i]
                 Z_n.loc[(k, j), m]= C.loc[k, i].n
                 Z_s.loc[(k, j), m]= C.loc[k, i].s
 
@@ -97,18 +99,27 @@ for m in np.arange(1, pocet_neznamych+1):
 X=Z_n
 y=-M
 
+X2=Z
+y2=-M
+
 #rucne
-prutoky1=np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
+I1=np.linalg.inv(np.dot(X.T, X))
+pom=unumpy.matrix(np.dot(X2.T, X2))
+I2=pom.I
+
+prutoky1=np.dot(I1, np.dot(X.T, y))
+prutoky2=np.dot(I2, np.dot(X2.T, y)) #toto je s nejistotami
+prutoky2=np.array(prutoky2)[0]
 
 #scipy lsq_linear
-prutoky2=lsq_linear(X, y)
-prutoky2_n=prutoky2.x
+# prutoky2=lsq_linear(X, y)
+# prutoky2_n=prutoky2.x
 
 #statsmodel
 res_ols=sm.OLS(y, X).fit()
 print(res_ols.summary())
 prutoky3_n=res_ols.params
-prutoky3_s=res_ols.bse
+# prutoky3_s=res_ols.bse
 
 #index pro dataframe, ktery se bude exportovat
 prutoky_indexy_modified=[]
@@ -120,9 +131,24 @@ for i in np.arange(1, len(prutoky_indexy)+1):
                                         str(prutoky_indexy.loc[i, 'cilova zona']))
 
 #export
-prutoky_hodnoty2=pd.DataFrame(np.array([prutoky3_n, prutoky3_s]).T, index=prutoky_indexy_modified,
+# prutoky_hodnoty2=pd.DataFrame(np.array([prutoky3_n, prutoky3_s]).T, index=prutoky_indexy_modified,
+                            # columns=['R', 'uR'])
+prutoky_hodnoty2=pd.DataFrame(np.array([unumpy.nominal_values(prutoky2),
+                                        unumpy.std_devs(prutoky2)]).T,
+                              index=prutoky_indexy_modified,
                             columns=['R', 'uR'])
 prutoky_hodnoty2.index.name='ozn'
 prutoky_hodnoty2=prutoky_hodnoty2.sort_index()
 
 prutoky_hodnoty2.to_csv('airflows.txt', sep=';', decimal=',', float_format='%.8f')
+
+# odezva_TD_df=odezva_TD.loc[:, ['jmeno', 'R_d', 'R_d_err']]
+# odezva_TD_df=odezva_TD_df.reset_index()
+# odezva_TD_df.index=[odezva_TD_df.loc[:, 'jmeno'], odezva_TD_df.loc[:, 'zona']]
+# odezva_TD_df.loc[:,'R_d']=unumpy.uarray(odezva_TD_df.loc[:,'R_d'], odezva_TD_df.loc[:,'R_d_err'])
+# odezva_TD_df=odezva_TD_df.drop(columns=['jmeno', 'zona', 'plyn', 'R_d_err'])
+
+# def f(x):
+    # return '{:.1f}'.format(x)
+
+# odezva_TD_df.to_latex('odezvy_TD.tex', decimal=',',formatters=[f], escape=False, column_format='lrr')
